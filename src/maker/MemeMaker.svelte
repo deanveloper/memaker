be2f9c0de41ff9d258c7819e06fdd5cee244e5e8
Dean Bassett <dean@dbassett.dev>
2021-11-15 08:35:59 -0800

    add text layers

--- src/maker/MemeMaker.svelte
+++ src/maker/MemeMaker.svelte
@@ -1,18 +1,38 @@
 <script>
 import LayerWindow from "./LayerWindow.svelte";
-
+	let selected = undefined;
+	let editCurrentLayer = false;
 	let layers = [
-		{ type: "img", src: "https://i.imgur.com/9iwuJCU.jpeg" }
+		{ type: "image", value: "https://i.imgur.com/9iwuJCU.jpeg", loaded: false },
+		{ type: "text", value: "some sample text" }
 	];
+
+	function onLayerClick(i) {
+		if (i != selected) {
+			editCurrentLayer = false;
+			selected = i;
+		}
+	}
+	function onLayerDblClick() {
+		editCurrentLayer = true;
+	}
+
+	$: if (editCurrentLayer && selected === undefined) {
+		editCurrentLayer = false;
+	}
 </script>
 
-<div class="maker">
-	{#each layers as layer}
-		<LayerWindow selected={layer.selected} on:click={ev => layer.selected = true}>
-		{#if layer.type === 'img'}
-			<img alt="" src={layer.src} />
+<div class="maker" on:click={() => selected = undefined}>
+	{#each layers as layer, i}
+		{#if layer.type === 'image'}
+			<LayerWindow loaded={layer.loaded === true} selected={selected === i} on:click={() => onLayerClick(i)} on:dblclick={onLayerDblClick}>
+				<img class='layer' alt="" src={layer.value} on:load={() => layer.loaded = true} />
+			</LayerWindow>
+		{:else if layer.type === 'text'}
+			<LayerWindow loaded={true} selected={selected === i} editing={selected === i && editCurrentLayer} on:click={() => onLayerClick(i)} on:dblclick={onLayerDblClick}>
+				<span class='layer' contenteditable={selected === i && editCurrentLayer}>{layer.value}</span>
+			</LayerWindow>
 		{/if}
-		</LayerWindow>
 	{/each}
 </div>
 
@@ -24,5 +44,9 @@ import LayerWindow from "./LayerWindow.svelte";
 
 	border: 1px solid red;
 	overflow: hidden;
+}
+.layer {
+	margin: 0;
+	padding: 0;
 }
 </style>
